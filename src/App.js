c3c10f7be17ae2493a215df876a5311ba5cf77ef
hamzajalal <hamza.jalal2@myy.haaga-helia.fi>
2018-11-01 17:41:58 +0200

    Added nav components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Navbar from './components/layoutComponents/Navbar';
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>Project Creator App</h1>
+          <Navbar />
         </div>
       </BrowserRouter>
     );
---
+++ src/components/layoutComponents/SignedInLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = () => {
+    return (
+        <ul className="right">
+            <li> <NavLink to='/'>New Project</NavLink> </li>
+            <li> <NavLink to='/'>Log Out</NavLink> </li>
+            <li> <NavLink to='/' className='btn btn-floating blue lighten-1'>HJ</NavLink> </li>
+        </ul>
+    )
+}
+
+export default SignedInLinks;
