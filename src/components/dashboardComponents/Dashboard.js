295e90a74582adf7f9a9ae217ee14b6a7594659c
hamzajalal <hamza.jalal2@myy.haaga-helia.fi>
2018-11-04 16:47:31 +0200

    Grabbing real data from database firestore

--- src/components/dashboardComponents/Dashboard.js
+++ src/components/dashboardComponents/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projectsComponents/ProjectList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class Dashboard extends Component {
     render () {
@@ -22,8 +24,16 @@ class Dashboard extends Component {
     }
 }
 
-const mapStateToProps = state => ({
-        projects: state.project.projects
-});
+const mapStateToProps = state => {
+    console.log(state);
+    return {
+        projects: state.firestore.ordered.projects
+    }
+};
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(Dashboard);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authenticationReducer from './authenticationReducer';
 import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer= combineReducers({
     authentication: authenticationReducer,
-    project: projectReducer
+    project: projectReducer,
+    firestore: firestoreReducer,
 });
 
 export default rootReducer;
