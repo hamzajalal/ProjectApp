f0ac69146e14621d31d7a5d7955ef487c95f3123
hamzajalal <hamza.jalal2@myy.haaga-helia.fi>
2018-11-04 11:23:53 +0200

    Added async call method for database dispatch

--- src/components/projectsComponents/CreateProject.js
+++ src/components/projectsComponents/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { createProject } from '../../store/actions/projectActions';
+import { connect } from 'react-redux';
 
 class CreateProject extends Component {
     state = {
@@ -12,7 +14,8 @@ class CreateProject extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        //console.log(this.state)
+        this.props.createProject(this.state)
     }
   render() {
     return (
@@ -36,4 +39,11 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(
+    null, mapDispatchToProps)(CreateProject);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,10 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+        console.log('created project', action.project)
+    }
     return state
 }
 
