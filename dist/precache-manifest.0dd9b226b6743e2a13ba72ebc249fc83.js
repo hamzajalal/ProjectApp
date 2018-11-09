c656c4989a8ff9f2dfd1a118bd0d0d4d1dda75da
hamzajalal <hamza.jalal2@myy.haaga-helia.fi>
2018-11-09 16:31:47 +0200

    Added Notifications to frontend and backend and deployed the full project on Firebase hosting

---
+++ dist/precache-manifest.0dd9b226b6743e2a13ba72ebc249fc83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8e00399deadd8983370",
+    "url": "/static/js/main.e8e00399.chunk.js"
+  },
+  {
+    "revision": "f0697064650e69a4b2bc",
+    "url": "/static/js/1.f0697064.chunk.js"
+  },
+  {
+    "revision": "e8e00399deadd8983370",
+    "url": "/static/css/main.beb1b906.chunk.css"
+  },
+  {
+    "revision": "02b87a0f782e94f1e60ba7e1c1e56873",
+    "url": "/index.html"
+  }
+];
--- src/components/dashboardComponents/Dashboard.js
+++ src/components/dashboardComponents/Dashboard.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom';
 class Dashboard extends Component {
     render () {
         // console.log(this.props);
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
         if (!auth.uid) return <Redirect to='/signin' />
 
         return (
@@ -19,7 +19,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications}/>
                     </div>
                 </div>
             </div>
@@ -31,13 +31,15 @@ const mapStateToProps = state => {
     console.log(state);
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 };
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects' }
+        { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+        { collection: 'notifications', limit: 4, orderBy: ['time', 'desc']}
     ])
 )(Dashboard);
